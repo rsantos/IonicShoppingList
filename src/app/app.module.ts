fc1900e90b5e54ae89a1b5128358ef96294af9bb
Rafael <rsanthos@gmail.com>
2017-10-25 10:56:51 -0200

    adding angularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [MyApp],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
+  ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp],
   providers: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBvyrRDcqxOrNh1a8ue8A8VdjRSNT18T5Q",
+  authDomain: "ionicshoppinglist-75a94.firebaseapp.com",
+  databaseURL: "https://ionicshoppinglist-75a94.firebaseio.com",
+  projectId: "ionicshoppinglist-75a94",
+  storageBucket: "ionicshoppinglist-75a94.appspot.com",
+  messagingSenderId: "178548866294"
+};
